76480089aab1d0f80676d537f72d7762d6774810
Jeremy Hong <hhw5321@gmail.com>
2021-05-03 16:45:32 +0900

    5/3 Log-out implemented

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,38 @@
 import React, {useState} from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "routes/Auth";
-import Home from "routes/Home";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
+import       Auth from "routes/Auth";
+import       Home from "routes/Home";
+import    Profile from "routes/Profile";
+import Navigation from "components/Navigation";
+
+
+/*
+isLoggedIn && <Navigation /> --> 무조건 true여야만 Navagation component 출력함
+*/
 
 const AppRouter = ({ isLoggedIn }) => { 
     return (
     <Router>
+        {isLoggedIn && <Navigation />}
         <Switch>
             {isLoggedIn ? (
                 <>
-                <Route exact path="/">
-                 <Home />
-                </Route>
+                    <Route exact path="/">
+                    <Home />
+                    </Route>
+                    <Route exact path="/profile">
+                    <Profile />
+                    </Route>
+                    <Redirect from="*" to="/" /> 
                 </>
             ) : (
-            <Route> 
-                <Auth exact path="/"/> 
-            </Route> 
+            <>
+                <Route> 
+                    <Auth exact path="/"/> 
+                </Route> 
+                <Redirect from="*" to="/" />
+            </>
             )}
         </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,9 @@
+import { authService } from "myBase";
 import React from "react";
 
-export default () => <span>Profile</span>;
+export default () => {
+    const onLogOutClick = () => authService.signOut();
+    return <>
+        <button onClick={onLogOutClick}>Log Out</button>
+        </>; 
+};
