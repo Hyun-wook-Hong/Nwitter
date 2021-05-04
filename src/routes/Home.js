08a85ac20c3f6398bd1cf114240f4b9a9af6a6e4
Jeremy Hong <hhw5321@gmail.com>
2021-05-04 11:06:24 +0900

    5/4 nweeting form add in Home.js and set DB start

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import { authService, firebaseInstance } from "../myBase";
 
-const Home= () => <span>Home</span>;
+// Tweet을 날릴 수 있는 Home route -> Firebase DB와 연동
+
+const Home= () => {
+    const [nweet, setNweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const onChange = (event) =>{
+        const {
+             target:{value}, 
+            } = event;
+            setNweet(value);
+    };
+
+    return(
+    <div>
+        <form onSubmit={ onSubmit }>
+            <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+            <input type="submit" value="Nweet"/>
+        </form>
+    </div>
+    );
+    };
 export default Home;
